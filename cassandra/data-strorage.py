4c9e94a8f5214d5c45fa26b81420e3b23fe45312
Yuchang Chen <yc1181@nyu.edu>
2016-08-26 23:42:21 -0400

    Update data-strorage.py

--- cassandra/data-strorage.py
+++ cassandra/data-strorage.py
@@ -1,66 +1,63 @@
-from googlefinance import getQuotes
-from kafka import KafkaProducer
-from kafka.errors import KafkaError, KafkaTimeoutError
+from cassandra.cluster import Cluster
+from kafka import KafkaConsumer
+from kafka.errors import KafkaError
 
-import time
 import atexit
 import argparse
 import logging
 import json
-import schedule
 
 topic_name = ''
 kafka_broker = ''
+key_space = ''
+data_table = ''
+cassandra_broker = ''
 
 logger_format = '%(asctime)-15s %(message)s'
 logging.basicConfig(format = logger_format)
-logger = logging.getLogger('data-producer')
+logger = logging.getLogger('data-storage')
 logger.setLevel(logging.INFO)
 
-# - TRACE DEBUG INFO WARN ERROR
 
-def fetch_price(producer, symbol):
-	price = json.dumps(getQuotes(symbol));
-	logger.debug('Get stock price %s', price)
-	try:
-		producer.send(topic = topic_name, value = price, timestamp_ms = time.time())
-	except Exception:
-		logger.warn('Failed to send msg to kafka')
-	looger.debug('Successfully Sent data to Kafka')
+def save_data(msg, cassandra_session):
+	parsed = json.loads(msg)[0]  # transfer json to type python acceptable
+	symbol = parsed.get('StockSymbol')
+	tradeprice = float(parsed.get('LastTradePrice'))
+	tradetime = parsed .get('LastTradeDateTime')
 
-def shutdown_hook(producer):
-	looger.info('preparing to shutdown, waiting for producer to flush msg')
-	producer.flush(10)
-	logger.info('producer flush finished')
-	try:
-		producer.close()
-	except Exception:
-		logger.warn('producer failed to close')
-	logger.info('producer closed')
+	logger.info('received data from Kafka %s', parsed)
+
+	# - use CQL statement to insert data
+	statement = "INSERT INTO %s (stock_symbol, trade_time, trade_price) VALUES ('%s', '%s', %f)" % (data_table, symbol, tradetime, tradeprice)
+	cassandra_session.execute(statement)
+	logger.info('Saved data to cassandra, symbol: %s, tradetime: %s, tradeprice: %f' % (symbol, tradetime, tradeprice))
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('symbol', help = 'the stock symbol')
-	parser.add_argument('topic_name', help = 'the kafka topic to push to')
-	parser.add_argument('kafka_broker', help = 'location of kafka broker')
+	parser.add_argument('topic_name', help = 'the kafka topic')
+	parser.add_argument('kafka_broker', help = 'the location of kafka broker')
+	parser.add_argument('key_space', help = 'the keyspace of cassandra')
+	parser.add_argument('data_table', help = 'the data table to use')
+	parser.add_argument('cassandra_broker', help = 'the cassandra location')
 
+	# - parse command line arguments
 	args = parser.parse_args()
-	symbol = args.symbol
 	topic_name = args.topic_name
 	kafka_broker = args.kafka_broker
+	key_space = args.key_space
+	data_table = args.data_table
+	# - 192.168.99.100.192.8687.789798.6786
+	cassandra_broker = args.cassandra_broker
 
-	producer = kafkaProducer(bootstrap_servers = kafka_broker)
+	# - setup kafka consumer
+	consumer = KafkaConsumer(topic_name, bootstrap_servers = kafka_broker)
 
-	# - schedule and run every second
-	schedule.every(1).second.do()(fetch_price, producer, symbol)
-    
-	# - register shutdown hook
-	atexit.register(shutdown_hook, producer)
+	# - setup cassandra session
+	cassandra_cluster = Cluster(contact_points = cassandra_broker.split(','))
+	session = cassandra_cluster.connect(key_space)
 
-    # - kick start
-	while True:
-		schedule.run_pending()
-		time.sleep(1)
+	logger.info('Created Kafka Consumer and Cassandra Session')
 
-    # fetch_price(producer, symbol)
-
+	for msg in consumer:
+		# logger.info(msg.value)
+		save_data(msg.value, session)
