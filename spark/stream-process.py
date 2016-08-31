4ef1e2443d0ce2e3012d634004448370b94e22f3
Yuchang Chen <yc1181@nyu.edu>
2016-08-31 15:46:54 -0400

    Update stream-process.py

--- spark/stream-process.py
+++ spark/stream-process.py
@@ -15,10 +15,9 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
-topic_name = ''
-kafka_broker = ''
-key_space = ''
-data_table = ''
+kafka_broker = '192.168.99.100:9092'
+kafka_topic = 'stock-analyzer'
+new_topic = 'test'
 
 logger_format = '%(asctime)-15s %(message)s'
 logging.basicConfig(format = logger_format)
@@ -35,15 +34,25 @@ def process(timeobj, rdd):
 	# - for each rdd record, do sth (take out the LastTradePrice, json)
 	# - for all the rdd record, sum up -> reduce
 	price_sum = rdd.map(lambda record: float(json.loads(record[1].decode('utf-8'))[0].get('LastTradePrice'))).reduce(lambda a, b: a+b)
-	print(price_sum)
-	print(num_of_records)
+	# print(price_sum)
+	# print(num_of_records)
 	average = price_sum / num_of_records
 	logger.info('Received records from Kafka, average price is %f' % average)
 	current_time = time.time()
 	data = json.dumps({'timestamp': current_time, 'average': average})
 
 	# - new topic name: averagePrice
-	# kafka_producer.send(topic=topic_name, value=average, timestamp_ms=time.time())
+	kafka_producer.send(new_topic, value=data, timestamp_ms=time.time())
+
+def shutdown_hook(producer):
+	logger.info('preparing to shutdown, waiting for producer to flush msg')
+	producer.flush(10)
+	logger.info('producer flush finished')
+	try:
+		producer.close()
+	except Exception:
+		logger.warn('producer failed to close')
+	logger.info('producer closed')
 
 
 if __name__ == '__main__':
@@ -52,7 +61,7 @@ if __name__ == '__main__':
 	# - sys.argv[0] stream-process.py
 	# - sys.argv[1] broker location
 	# - sys.argv[2] topic
-	if (len(sys.argv) != 3):
+	if (len(sys.argv) != 4):
 		print("Not enough argument [kafka broker location], [kafka topic location], [kafka new topic location]")
 		exit(1)
 
@@ -61,15 +70,17 @@ if __name__ == '__main__':
 
 	ssc = StreamingContext(sc, 5)
 
-	kafka_topic, kafka_broker = sys.argv[1:]
+	kafka_broker, kafka_topic, new_topic = sys.argv[1:]
 
 	# - setup a kafka stream
+	# print(kafka_topic)
+	# print(kafka_broker)
 	directKafkaStream = KafkaUtils.createDirectStream(ssc, [kafka_topic], {'metadata.broker.list': kafka_broker})
 	directKafkaStream.foreachRDD(process)
 
-	# kafka_producer = KafkaProducer(bootstrap_servers = kafka_broker)
+	kafka_producer = KafkaProducer(bootstrap_servers = kafka_broker)
 
-	atexit.register()
-
+	atexit.register(shutdown_hook, kafka_producer)
+ 
 	ssc.start()
 	ssc.awaitTermination()
